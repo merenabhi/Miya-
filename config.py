49e037aa2c6644362aac6aa7eb861cf9bf241416
merenabhi <nsuzuka803@gmail.com>
2025-04-28 22:13:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "24731191"
+api_hash = "ca156f58ad03bb5e350c855bb82e9991"
+bot_token = "8072629482:AAHPe80GDhzNGaVx9TfKx6Imo2tLKqajvmA"
+auth_users = [7755066341]
+sudo_users = [7755066341]
